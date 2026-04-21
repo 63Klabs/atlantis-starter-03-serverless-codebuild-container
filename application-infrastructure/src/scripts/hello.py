9c5df02d3ffda0aaf2ac00b5317740c4a6165a69
chadkluck <kl5@chadkluck.net>
2026-04-21 09:31:24 -0500

    updated python script and permissions

--- application-infrastructure/src/scripts/hello.py
+++ application-infrastructure/src/scripts/hello.py
@@ -1,3 +1,28 @@
 # python3
 
-print("Hello, World! This is a Scheduled Job")
+def main():
+    horizontal_line = r"""
+-------------------------------------------------------------------------
+    """
+    hello_world = r"""
+ _   _  _____ _     _      ___        __        _____  ____  _     ____
+| | | || ____| |   | |    / _ \       \ \      / / _ \|  _ \| |   |  _ \
+| |_| ||  _| | |   | |   | | | |       \ \ /\ / / | | | |_) | |   | | | |
+|  _  || |___| |___| |___| |_| |  _     \ V  V /| |_| |  _ <| |___| |_| |
+|_| |_||_____|_____|_____|\___/  ( )     \_/\_/  \___/|_| \_\_____|____/
+                                 |/
+    """
+
+    about = r"""
+Hello, World!
+This is a scheduled job using CodeBuild
+Deployed using the 63Klabs Atlantis Template and Scripts Platform
+    """
+
+    print(horizontal_line)
+    print(hello_world)
+    print(about)
+    print(horizontal_line)
+
+if __name__ == "__main__":
+    main()
